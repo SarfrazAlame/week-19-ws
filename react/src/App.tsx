d604d94ef99ca0f725d033d279062430989e6e3f
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-04-12 10:12:24 +0530

    sdh

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,13 +2,31 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [socket, setSocket] = useState(null);
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage, setLatestMessage] = useState("");
 
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log("Connected");
+      setSocket(socket);
+    };
+    socket.onmessage = (message) => {
+      console.log("Recieved message: ", message.data);
+      setLatestMessage(message.data);
+    };
+
+    setSocket(socket);
   }, []);
 
-  return <></>;
+  if (!socket) {
+    return <div>Connecting to socket server...</div>;
+  }
+
+  return <>
+  <input></input>
+  <button onClick={()=>socket.send("Hello world")}>send</button>
+  {latestMessage}</>;
 }
 
 export default App;
